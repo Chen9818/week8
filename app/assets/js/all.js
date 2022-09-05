0531e67e6ac02112ea31bbe05be3e7e47c64fa6b
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-09-05 22:09:31 +0900

    add swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,19 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 24,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+  },
+
 });
