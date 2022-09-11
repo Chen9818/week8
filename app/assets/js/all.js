375c336611d0dd639899c8236b1ae50d6f2e02ff
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-09-11 22:35:20 +0900

    fix bug

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,25 +1,3 @@
-// $(function() {
-//   console.log('Hello Bootstrap5');
-// });
-
-
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   loop: true,
-//   centeredSlides: true,
-//   slidesPerView: 3,
-//   spaceBetween: 24,
-
-//   pagination: {
-//     el: '.swiper-pagination',
-//     dynamicBullets: true,
-//     clickable: true,
-//   },
-
-// });
-
-
-
 var swiper = new Swiper(".swiper", {
   slidesPerView: 3,
   spaceBetween: 150,
@@ -29,23 +7,17 @@ var swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  // breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 10
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20
-  //   },
-  //   // when window width is >= 640px
-  //   768: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30
-  //   }
-  // }
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 
 // init Masonry
