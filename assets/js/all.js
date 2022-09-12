de51d1bd7714e2ccb756947c0eae32577e6934bb
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-09-12 17:33:33 +0900

    Update 2022-09-12T08:33:09.552Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,31 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 150,
+  loop: true,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+}); // init Masonry
+
+var $grid = $('.grid').masonry({// options...
+}); // layout Masonry after each image loads
+
+$grid.imagesLoaded().progress(function () {
+  $grid.masonry();
 });
 //# sourceMappingURL=all.js.map
